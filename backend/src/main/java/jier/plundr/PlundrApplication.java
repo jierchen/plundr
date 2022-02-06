25b1df1aa1e802213026ddbf31163896a514b486
Jier <j793chen@uwaterloo.ca>
2022-02-06 17:44:38 -0500

    Add initial project files

---
+++ backend/src/main/java/jier/plundr/PlundrApplication.java
@@ -0,0 +1,13 @@
+package jier.plundr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlundrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlundrApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/jier/plundr/PlundrApplicationTests.java
@@ -0,0 +1,13 @@
+package jier.plundr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlundrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
