cc27f680df42d860d259dabac6243aa352c78343
Jier <j793chen@uwaterloo.ca>
2022-03-07 00:23:42 -0500

    Add DTO for Employee object

---
+++ backend/src/main/java/jier/plundr/dto/EmployeeDTO.java
@@ -0,0 +1,17 @@
+package jier.plundr.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class EmployeeDTO {
+
+    private BigDecimal salary;
+}
