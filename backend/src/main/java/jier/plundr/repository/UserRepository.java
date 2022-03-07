b30b4300e78e133df210b47dec6a70a4953fa7c1
Jier <j793chen@uwaterloo.ca>
2022-03-07 00:17:27 -0500

    Add UserRepository

---
+++ backend/src/main/java/jier/plundr/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package jier.plundr.repository;
+
+import jier.plundr.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
