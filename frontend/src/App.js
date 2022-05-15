85ee7e0da894f6769e05f9bfcdfaaa66f2eae18c
Jier Chen <j793chen@uwaterloo.ca>
2022-05-15 00:33:38 -0400

    Include routes and change to functional components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,29 @@
 import './App.css';
-import React, {Component} from 'react';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NavigationBar from './components/NavigationBar';
+import Landing from './components/Landing';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import Profile from './components/profile/Profile';
 
-class App extends Component {
-  render() {
-    return(
-      <BrowserRouter>
-        <Routes>
-        </Routes>
-      </BrowserRouter>
-    )
-  }
+function App() {
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+	return (
+		<BrowserRouter>
+			<div className="app">
+				<NavigationBar isLoggedIn={isLoggedIn} handleUserLogout={() => setIsLoggedIn(false)} />
+
+				<Routes>
+					<Route path="/" element={<Landing />} />
+					<Route path="/login" element={<Login handleUserLogin={() => setIsLoggedIn(true)} />} />
+					<Route path="/signup" element={<SignUp />} />
+					<Route path="/profile" element={<Profile isLoggedIn={isLoggedIn}/>} />
+				</Routes>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -1,20 +1,14 @@
-import React, { Component } from 'react';
-import {
-    Button,
-    Collapse,
-    Nav,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    NavItem,
-    NavLink,
-} from 'reactstrap';
+import React from 'react';
+import { Button, Collapse, Nav, Navbar, NavbarToggler, NavbarBrand, NavItem, NavLink } from 'reactstrap';
 import { useNavigate } from 'react-router-dom';
 
 function NavigationButton(props) {
     let navigate = useNavigate()
 
     function navigateToUrl() {
+        if(typeof props.preNavFunc === "function") {
+            props.preNavFunc();
+        }
         navigate(props.url)
     }
 
@@ -27,34 +21,40 @@ function NavigationButton(props) {
     );
 }
 
-class NavigationBar extends Component {
+function NavigationBar(props) {
 
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <Navbar className="navbar" color="dark" dark expand="md" fixed="top">
-                <NavbarBrand href="/">Plundr</NavbarBrand>
-                <NavbarToggler/>
-                <Collapse navbar>
-                    <Nav navbar>
-                        <NavItem>
-                            <NavLink>About</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="https://github.com/jchenuw">Author</NavLink>
-                        </NavItem>
-                    </Nav>
-                </Collapse>
-                <Nav navbar className="gap-3">
-                    <NavigationButton title="Login" color="primary" url="/login" />
-                    <NavigationButton title="Sign Up" color="secondary" url="/signup" />
-                </Nav>
-            </Navbar>
+    let navEnd;
+    
+    if (props.isLoggedIn) {
+        navEnd = <NavigationButton title="Logout" color="primary" url="/" preNavFunc={() => props.handleUserLogout()} />
+    } else {
+        navEnd = (
+            <React.Fragment>
+                <NavigationButton title="Login" color="primary" url="/login" />
+                <NavigationButton title="Sign Up" color="secondary" url="/signup" />
+            </React.Fragment>
         );
     }
+
+    return (
+        <Navbar className="navbar" color="dark" dark expand="md">
+            <NavbarBrand href="/">💰 Plundr</NavbarBrand>
+            <NavbarToggler />
+            <Collapse navbar>
+                <Nav navbar>
+                    <NavItem>
+                        <NavLink>About</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="https://github.com/jchenuw">Author</NavLink>
+                    </NavItem>
+                </Nav>
+            </Collapse>
+            <Nav navbar className="gap-3">
+                {navEnd}
+            </Nav>
+        </Navbar>
+    );
 }
 
 export default NavigationBar;
