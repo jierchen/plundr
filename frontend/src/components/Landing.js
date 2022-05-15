f0ef4393d9560bde5433a8ea14e8f8b1a37153c2
Jier Chen <j793chen@uwaterloo.ca>
2022-05-15 01:25:07 -0400

    Add landing page

---
+++ frontend/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Landing.css';
+
+function Landing() {
+    return (
+        <section className="landing-section">
+            <div className="landing-section-content">
+                 💰
+             </div>
+        </section>
+    );
+}
+
+export default Landing;
